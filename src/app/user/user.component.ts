79ed4dd930bf3076408536d6203aca3123c2f2cd
Antony <antonysameh1999@gmail.com>
2024-12-09 17:14:21 +0200

    ok

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+import
 @Component({
   selector: 'app-user',
   imports: [],
+  standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = 
 }
